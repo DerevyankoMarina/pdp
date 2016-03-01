d99f7c333723b21c117d22baed414f6273e650b5
Maryna Derevianko <Maryna_Derevianko@epam.com>
2016-03-01 13:22:30 +0200

    Edit gulpfile, add some markup

--- markup/gulpfile.js
+++ markup/gulpfile.js
@@ -1,23 +1,40 @@
 // include the required packages.
 var gulp = require('gulp');
 var stylus = require('gulp-stylus');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
 var watch = require('gulp-watch');
+var del = require('del');
 
 gulp.task('default', [
+  //'copy',
+  'clean',
   'convertCss',
   'watcher'
 ]);
 
-gulp.task('convertCss', function () {
-  return gulp.src('./css/all.styl')
-    .pipe(stylus())
-    .pipe(gulp.dest('./build'));
+gulp.task('clean', function () {
+  return del('build')
 });
 
+gulp.task('convertCss', function () {
+  return gulp.src('./css/all.styl')
+    .pipe(sourcemaps.init())
+    .pipe(stylus())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./build/css'));
+});
+
+//gulp.task('copy', function () {
+//  return gulp.src('./fonts/**')
+//      .pipe(gulp.dest('./build'));
+//});
+
+
 gulp.task('watcher', function(){
-  watch(['./css/all.styl'], function(event, cb) {
+  watch(['./css/**/*.*'], function(event, cb) {
     gulp.start('convertCss');
-  });
+  })
 });
 
 
@@ -25,3 +42,8 @@ gulp.task('watcher', function(){
 // https://www.npmjs.com/package/gulp-watch
 // https://www.youtube.com/watch?v=Kh4eYdd8O4w&list=PLLnpHn493BHE2RsdyUNpbiVn-cfuV7Fos&index=1
 // https://habrahabr.ru/post/208890/
+
+
+// https://www.npmjs.com/package/gulp-autoprefixer
+
+
